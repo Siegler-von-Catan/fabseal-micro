ae40ad2273e5f7a475f776734348571538dfd382
Tobias Markus <tobias@miglix.eu>
2021-08-24 18:47:50 +0200

    worker: Improved error handling

--- worker-blender/src/main.rs
+++ worker-blender/src/main.rs
@@ -18,67 +18,118 @@ use std::path::PathBuf;
 
 use redis::Commands;
 
-async fn handle_work_item(
-    delivery: lapin::message::Delivery,
-    conn: &mut redis::Connection
-)
+use anyhow::Result;
+
+mod settings;
+use settings::Settings;
+
+struct Context {
+    blend_path: PathBuf,
+    python_path: PathBuf,
+}
+impl Context {
+    fn from_dmstl_dir(dmstl_path: &str) -> Context {
+        let dmstl: PathBuf  = PathBuf::from(dmstl_path).canonicalize().unwrap();
+
+        let blend_path: PathBuf = {
+            let mut p = dmstl.clone();
+            p.push(r"src/empty.blend");
+            p
+        };
+        let python_path: PathBuf = {
+            let mut p = dmstl;
+            p.push(r"src/displacementMapToStl.py");
+            p
+        };
+
+        Context {
+            blend_path,
+            python_path
+        }
+    }
+}
+
+async fn try_handle(
+    payload: &ImageRequest,
+    conn: &mut redis::Connection,
+    ctx: &Context
+) -> Result<()>
 {
-    let payload: ImageRequest = serde_json::from_slice(&delivery.data).unwrap();
-    info!("payload={:?}", payload.image.image_type);
-
-    let dmstl_env = std::env::var("DMSTL_DIR").expect("Specify displacementMapToStl directory with DMSTL_DIR");
-    let dmstl: PathBuf  = PathBuf::from(dmstl_env).canonicalize().unwrap();
-
-    let mut input_file = NamedTempFile::new().unwrap();
-    let mut output_file = NamedTempFile::new().unwrap();
+    let mut input_file = NamedTempFile::new()?;
+    let mut output_file = NamedTempFile::new()?;
 
     debug!("input temp file: {:?}", input_file);
     debug!("output temp file: {:?}", output_file);
 
-    input_file.write_all(&payload.image.image).unwrap();
-
-    let mut blend_path: PathBuf = dmstl.clone();
-    blend_path.push(r"src/empty.blend");
-    let mut python_path: PathBuf = dmstl.clone();
-    python_path.push(r"src/displacementMapToStl.py");
+    input_file.write_all(&payload.image.image)?;
 
     let mut comm = Command::new("blender");
     comm
             .arg("--threads").arg("4")
             .arg("--background")
-            .arg(blend_path.as_os_str())
+            .arg(ctx.blend_path.as_os_str())
             .arg("--python")
-            .arg(python_path.as_os_str())
+            .arg(ctx.python_path.as_os_str())
             .arg("--")
             .arg(input_file.path())
             .arg(output_file.path());
 
     debug!("the command: {:?}", comm);
 
-    let _status = comm
-            .status()
-            .expect("failed to execute process");
+    let _status = comm.status()?;
 
     let mut result_data: Vec<u8> = Vec::new();
-    output_file.as_file_mut().read_to_end(&mut result_data).unwrap();
+    output_file.as_file_mut().read_to_end(&mut result_data)?;
 
     let key = result_key(payload.request_id);
     debug!("setting key={}", key);
-    let _ : () = conn.set_ex(key, result_data, RESULT_EXPIRATION_SECONDS).unwrap();
+    let _ : () = conn.set_ex(key, result_data, RESULT_EXPIRATION_SECONDS)?;
 
     debug!("closing temporary files");
-    input_file.close().unwrap();
-    output_file.close().unwrap();
+    input_file.close()?;
+    output_file.close()?;
 
-    debug!("ack-ing message");
+    Ok(())
+}
 
-    delivery
-        .ack(BasicAckOptions::default())
-        .await
-        .expect("ack failed");
+async fn handle_work_item(
+    delivery: lapin::message::Delivery,
+    conn: &mut redis::Connection,
+    ctx: &Context
+)
+{
+    let payload: ImageRequest = serde_json::from_slice(&delivery.data).unwrap();
+    info!("payload={:?}", payload.image.image_type);
+
+    match try_handle(&payload, conn, ctx).await {
+        Ok(_) => {
+            debug!("ack-ing message");
+
+            delivery
+                .ack(BasicAckOptions::default())
+                .await
+                .expect("ack failed");
+        },
+        Err(e) => {
+            info!("error: {}", e);
+            debug!("nack-ing message");
+
+            delivery
+                .nack(BasicNackOptions::default())
+                .await
+                .expect("nack failed");
+        }
+    }
+
 }
 
 fn run() {
+    let settings = Settings::new().unwrap();
+    debug!("settings: {:?}", settings);
+
+    let dmstl_env = std::env::var("DMSTL_DIR").expect("Specify displacementMapToStl directory with DMSTL_DIR");
+    let ctx = Context::from_dmstl_dir(dmstl_env.as_str());
+
     info!("will launch");
     let rmq_addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
     let redis_addr = std::env::var("REDIS_ADDR").unwrap_or_else(|_| "redis://127.0.0.1/".into());
@@ -111,7 +162,7 @@ fn run() {
             while let Some(delivery) = consumer.next().await {
                 info!("received delivery");
                 let (_, delivery) = delivery.expect("error in consumer");
-                handle_work_item(delivery, &mut redis_conn).await;
+                handle_work_item(delivery, &mut redis_conn, &ctx).await;
             }
         }).await;
 
