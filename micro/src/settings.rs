211556d53903358340c220bea083a6972c0d6baf
Tobias Markus <tobias@miglix.eu>
2021-08-27 17:57:11 +0200

    Update settings struct

--- micro/src/settings.rs
+++ micro/src/settings.rs
@@ -3,11 +3,6 @@ use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct AmqpSettings {
-    pub address: String,
-}
-
-#[derive(Debug, Deserialize, Clone)]
 pub struct RedisSettings {
     pub address: String,
     pub db_id: Option<i64>,
@@ -19,7 +14,6 @@ pub struct Settings {
     pub debug: bool,
     pub http_endpoint: String,
     pub domain: Option<String>,
-    pub amqp: AmqpSettings,
     pub redis: RedisSettings,
 }
 
@@ -45,10 +39,6 @@ impl Settings {
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         s.merge(Environment::with_prefix("fabseal"))?;
-
-        // Now that we're done, let's access our configuration
-        println!("debug: {:?}", s.get_bool("debug"));
-        println!("database: {:?}", s.get::<String>("database.url"));
 
         // You can deserialize (and thus freeze) the entire configuration as
         s.try_into()
