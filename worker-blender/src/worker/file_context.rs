7994b6dbc79ed4819aea45a947a0ce0ca4dc5c31
Tobias Markus <tobias@miglix.eu>
2021-08-27 19:30:59 +0200

    worker: Use TempPath for CommandFileContext

--- worker-blender/src/worker/file_context.rs
+++ worker-blender/src/worker/file_context.rs
@@ -1,17 +1,16 @@
-use tempfile::NamedTempFile;
+use tempfile::{NamedTempFile, TempPath};
 
 use std::{io::Write, path::Path};
 
-use anyhow::Result;
 use log::{debug, trace};
 
 pub(crate) struct CommandFileContext {
-    input_file: NamedTempFile,
-    output_file: NamedTempFile,
+    input_file: TempPath,
+    output_file: TempPath,
 }
 
 impl CommandFileContext {
-    pub(crate) fn create(input_data: &[u8]) -> Result<CommandFileContext> {
+    pub(crate) fn create(input_data: &[u8]) -> std::io::Result<CommandFileContext> {
         let mut input_file = NamedTempFile::new()?;
         let output_file = NamedTempFile::new()?;
 
@@ -21,21 +20,21 @@ impl CommandFileContext {
         input_file.write_all(input_data)?;
 
         Ok(CommandFileContext {
-            input_file,
-            output_file,
+            input_file: input_file.into_temp_path(),
+            output_file: output_file.into_temp_path(),
         })
     }
 
     pub(crate) fn input_file_path(&self) -> &Path {
-        self.input_file.path()
+        self.input_file.as_ref()
     }
 
     pub(crate) fn output_file_path(&self) -> &Path {
-        self.output_file.path()
+        self.output_file.as_ref()
     }
 
-    pub(crate) fn finish(self) -> Result<Vec<u8>> {
-        let result_data = std::fs::read(self.output_file.path())?;
+    pub(crate) fn finish(self) -> std::io::Result<Vec<u8>> {
+        let result_data = std::fs::read(&self.output_file)?;
 
         debug!("closing temporary files");
         self.input_file.close()?;
